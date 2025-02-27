07c0235aa555c9857de2f5b5063f798dc8d05697
JG <jacostag@users.noreply.github.com>
2025-02-27 16:24:01 -0500

    add gcloud to shared.sh
    rename functions and add functions for detect gcloud connections

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
 get_tmux_option() {
-	local option=$1
-	local default_value=$2
-	local option_value=$(tmux show-option -gqv "$option")
-	if [ -z "$option_value" ]; then
-		echo "$default_value"
-	else
-		echo "$option_value"
-	fi
+  local option=$1
+  local default_value=$2
+  local option_value=$(tmux show-option -gqv "$option")
+  if [ -z "$option_value" ]; then
+    echo "$default_value"
+  else
+    echo "$option_value"
+  fi
 }
 
 set_tmux_option() {
-	local option=$1
-	local value=$2
-	tmux set-option -gq "$option" "$value"
+  local option=$1
+  local value=$2
+  tmux set-option -gq "$option" "$value"
 }
 
 parse_ssh_port() {
   # If there is a port get it
-  local port=$(echo $1|grep -Eo '\-p\s*([0-9]+)'|sed 's/-p\s*//')
+  local port=$(echo $1 | grep -Eo '\-p\s*([0-9]+)' | sed 's/-p\s*//')
 
   if [ -z $port ]; then
     local port=22
@@ -31,10 +31,10 @@ parse_ssh_port() {
 get_ssh_user() {
   local ssh_user=$(whoami)
 
-  for ssh_config in `awk '
+  for ssh_config in $(awk '
     $1 == "Host" {
-      gsub("\\\\.", "\\\\.", $2);
-      gsub("\\\\*", ".*", $2);
+      gsub("\\.", "\\.", $2);
+      gsub("\\*", ".*", $2);
       host = $2;
       next;
     }
@@ -42,7 +42,7 @@ get_ssh_user() {
       $1 = "";
       sub( /^[[:space:]]*/, "" );
       printf "%s|%s\n", host, $0;
-    }' .ssh/config`; do
+    }' .ssh/config); do
     local host_regex=${ssh_config%|*}
     local host_user=${ssh_config#*|}
     if [[ "$1" =~ $host_regex ]]; then
@@ -54,50 +54,94 @@ get_ssh_user() {
   echo $ssh_user
 }
 
-get_remote_info() {
+get_remote_ssh() {
   local command=$1
 
   # First get the current pane command pid to get the full command with arguments
-  local cmd=$({ pgrep -flaP `tmux display-message -p "#{pane_pid}"` ; ps -o command -p `tmux display-message -p "#{pane_pid}"` ; } | xargs -I{} echo {} | grep ssh | sed -E 's/^[0-9]*[[:blank:]]*ssh //')
+  local cmd=$({
+    pgrep -flaP $(tmux display-message -p "#{pane_pid}")
+    ps -o command -p $(tmux display-message -p "#{pane_pid}")
+  } | xargs -I{} echo {} | grep ssh | sed -E 's/^[0-9]*[[:blank:]]*ssh //')
 
   local port=$(parse_ssh_port "$cmd")
 
-  local cmd=$(echo $cmd|sed 's/\-p\s*'"$port"'//g')
-  local user=$(echo $cmd | awk '{print $NF}'|cut -f1 -d@)
-  local host=$(echo $cmd | awk '{print $NF}'|cut -f2 -d@)
+  local cmd=$(echo $cmd | sed 's/\-p\s*'"$port"'//g')
+  local user=$(echo $cmd | awk '{print $NF}' | cut -f9 -d@)
+  local host=$(echo $cmd | awk '{print $NF}' | cut -f1 -d@)
 
   if [ $user == $host ]; then
     local user=$(get_ssh_user $host)
   fi
 
   case "$1" in
-    "whoami")
-      echo $user
-      ;;
-    "hostname")
-      echo $host
-      ;;
-    "port")
-      echo $port
-      ;;
-    *)
-      echo "$user@$host:$port"
-      ;;
+  "whoami")
+    echo $user
+    ;;
+  "hostname")
+    echo $host
+    ;;
+  "port")
+    echo $port
+    ;;
+  *)
+    echo "$user@$host:$port"
+    ;;
   esac
 }
 
+get_remote_gcloud() {
+  #  local command=$1
+  local parent=$(pgrep -P $(tmux display-message -p "#{pane_pid}"))
+  local grandparent=$(pgrep -P $parent)           # user
+  local greatgrandparent=$(pgrep -P $grandparent) #port and host
+
+  local user=$(ps -o command -p $grandparent | awk '{print $33}' | cut -f1 -d@)
+  local host=$(ps -o command -p $greatgrandparent | awk '{print $7}')
+  local port=$(ps -o command -p $greatgrandparent | awk '{print $8}')
+
+  case "$1" in
+  "whoami")
+    echo $user
+    ;;
+  "hostname")
+    echo $host
+    ;;
+  "port")
+    echo $port
+    ;;
+  *)
+    echo "$user@$host:$port"
+    ;;
+  esac
+}
+
+get_remote_mosh() {
+  echo "$user@$host:$port"
+}
+
 get_info() {
-  # If command is ssh do some magic
   if ssh_connected; then
-    echo $(get_remote_info $1)
+    echo $(get_remote_ssh $1)
+  elif mosh_connected; then
+    echo $(get_remote_mosh $1)
+  elif gcloud_connected; then
+    echo $(get_remote_gcloud $1)
   else
     echo $($1)
   fi
 }
 
 ssh_connected() {
-  # Get current pane command
   local cmd=$(tmux display-message -p "#{pane_current_command}")
-
   [ $cmd = "ssh" ] || [ $cmd = "sshpass" ]
+}
+
+gcloud_connected() {
+  local cmd=$(tmux display-message -p "#{pane_current_command}")
+  [ $cmd = "python" ]
+}
+
+mosh_connected() {
+  local cmd=$(tmux display-message -p "#{pane_current_command}")
+  [ $cmd = "mosh" ]
 }
