0228fcf19de9857c00a0b6f18eebb181d5d7ce83
JG <jacostag@users.noreply.github.com>
2025-02-27 16:20:37 -0500

    add mor options to pane_ssh_connected.sh
    added gcloud and a holder for mosh

--- scripts/pane_ssh_connected.sh
+++ scripts/pane_ssh_connected.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source $CURRENT_DIR/shared.sh
 
 main() {
   if ssh_connected; then
-      echo 1
+    echo "SSH"
+  elif gcloud_connected; then
+    echo "GC"
+  elif mosh_connected; then
+    echo "MOSH"
   else
-      echo 0
+    echo ""
   fi
 }
 
