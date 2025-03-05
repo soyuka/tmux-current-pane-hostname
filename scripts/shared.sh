3a688fa0ac4f3526ce22335a3ca3a3f8310722ea
Tony-Sol <tony.kent.nar.earth@gmail.com>
2025-03-06 02:13:09 +0300

    fixes and improvements:
    - fixed podman/docker compatibility
    - improved getting current pane command
    - improved nested ssh session detection

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -13,43 +13,19 @@ set_tmux_option() {
 	tmux set-option -gq "$option" "$value"
 }
 
-get_remote_info() {
-	# First get the current pane command pid to get the full command with arguments
-	local cmd=$(_current_pane_command | grep -E 'ssh' | sed -E 's/^[0-9]*[[:blank:]]*ssh //')
-	# Fetch configuration with given cmd
-	ssh -G $cmd 2>/dev/null | grep -E -e '^hostname\s' -e '^port\s' -e '^user\s' | sort | cut -f 2 -d ' ' | xargs
-}
-
-get_container_info() {
-	local cmd=$(_current_pane_command | grep -E 'docker|podman' | sed -E 's/^[0-9]*[[:blank:]]* //')
-
-	local runner=${cmd%% *}
-	if [[ $cmd =~ ' --name' ]]; then
-		local container=$(echo ${cmd##* --name} | cut -f 1 -d ' ')
-		container=${container##*=}
-	else
-		# @TODO get dynamic named container
-		# local all_running_containers=$($runner ps -q | xargs $runner inspect)
-		return
-	fi
-
-	local info=$($runner inspect --format '{{ .Config.Hostname }}/{{ .Config.Domainname }}/{{ .Config.User }}' $container)
-	local host=$(echo $info | cut -f 1 -d '/')
-	local domain=$(echo $info | cut -f 2 -d '/')
-	local user=$(echo $info | cut -f 3 -d '/')
-	# @TODO `port` is not applicable with container for now
-	echo "${host}${domain:+.$domain} 0 ${user}"
-}
-
 get_info() {
 	local host port user
-	# If command is ssh, fetch connection info
-	if ssh_connected; then
-		read -r host port user <<<$(get_remote_info)
+	local cmd=$(_current_pane_command)
+	if _ssh_cmd "$cmd"; then
+		cmd=$(echo "$cmd" | grep -E 'ssh' | sed -E 's/^.*ssh //')
+		read -r host port user <<<$(_get_remote_info "$cmd")
+	elif _containered_cmd "$cmd"; then
+		cmd=$(echo "$cmd" | grep -E 'docker|podman' | sed -E 's/^[[:blank:]]* //')
+		read -r host user <<<$(_get_container_info "$cmd")
+	else
+		read -r host user <<<$(_get_local_info)
 	fi
-	if containered; then
-		read -r host port user <<<$(get_container_info)
-	fi
+	#
 	# Return requested info
 	case "$1" in
 		"user") # user from ssh info or `whoami`
@@ -72,31 +48,90 @@ get_info() {
 
 ssh_connected() {
 	local cmd=$(_current_pane_command)
+	_ssh_cmd "$cmd"
+}
 
-	[[ $cmd =~ " ssh " ]] || [[ $cmd =~ " sshpass " ]]
+_ssh_cmd() {
+	[[ $1 =~ (^|^[[:blank:]]*|/?)ssh[[:blank:]] ]] || [[ $1 =~ (^|^[[:blank:]]*|/?)sshpass[[:blank:]] ]]
 }
 
 containered() {
 	local cmd=$(_current_pane_command)
+	_containered_cmd "$cmd"
+}
 
-	[[ $cmd =~ " docker " ]] || [[ $cmd =~ " podman " ]]
+_containered_cmd() {
+	[[ $1 =~ (^|^[[:blank:]]*|/?)docker[[:blank:]] ]] || [[ $1 =~ (^|^[[:blank:]]*|/?)podman[[:blank:]] ]]
 }
 
 _current_pane_command() {
-	local pane_pid=$(tmux display-message -p "#{pane_pid}")
-	local pane_process_pid=$(pgrep -flaP $pane_pid | cut -f 1 -d ' ')
+	local ppid=$(tmux display-message -p "#{pane_pid}")
+	local pid command cmd
 
-	ps -o command -p $pane_process_pid | grep -v 'COMMAND'
+	while [[ -n "$ppid" ]] ; do
+		read -r ppid pid command <<<$(ps -o ppid=,pid=,command= | grep -E "^[[:blank:]]*$ppid")
+		[[ -z "$command" ]] && break
+		ppid="$pid"
+		cmd="$command"
+	done
+
+	echo "$cmd"
 }
 
+_get_remote_info() {
+	local cmd="$1"
+	# Fetch configuration with given cmd
+	ssh -G $cmd 2>/dev/null | grep -E -e '^hostname\s' -e '^port\s' -e '^user\s' | sort | cut -f 2 -d ' ' | xargs
+}
+
+_get_container_info() {
+	local cmd="$1"
+
+	local runner=${cmd%% *}
+	if [[ $cmd =~ ' --name' ]]; then
+		local container=$(echo ${cmd##* --name} | cut -f 1 -d ' ')
+		container=${container##*=}
+	else
+		# @TODO get dynamic named container
+		# local all_running_containers=$($runner ps -q | xargs $runner inspect)
+		_get_local_info
+		return
+	fi
+
+	local format
+	case $runner in
+		'docker')
+			format='{{ .Config.Hostname }}/{{ .Config.Domainname }}/{{ .Config.User }}'
+			;;
+		'podman')
+			format='{{ .Config.Hostname }}/{{ .Config.DomainName }}/{{ .Config.User }}'
+			;;
+	esac
+
+	local info=$($runner inspect --format "$format" "$container")
+	local host=$(echo $info | cut -f 1 -d '/')
+	local domain=$(echo $info | cut -f 2 -d '/')
+	local user=$(echo $info | cut -f 3 -d '/')
+
+	echo "${host}${domain:+.$domain} ${user}"
+}
+
+_get_local_info() {
+	local user=$(_get_username)
+	local host=$(_get_hostname)
+
+	echo "${host} ${user}"
+}
+
+
 _get_username() {
-	command -v whoami && whoami
+	command -v whoami > /dev/null && whoami
 }
 
 _get_hostname() {
-	command -v hostname && hostname || echo "${HOSTNAME}"
+	command -v hostname > /dev/null && hostname || echo "${HOSTNAME}"
 }
 
 _get_hostname_short() {
-	command -v hostname && hostname --short || echo "${HOSTNAME%%.*}"
+	command -v hostname > /dev/null && hostname --short || echo "${HOSTNAME%%.*}"
 }
